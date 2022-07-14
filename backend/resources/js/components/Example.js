b0a39db6b633c44851f619c976f48be4e47c1a8e
yamanedesu <test@test.com>
2022-07-15 01:37:53 +0900

    【更新・削除】views配下app.blede以外削除、ルーティング・Example.jsを修正

--- backend/resources/js/components/Example.js
+++ backend/resources/js/components/Example.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Button} from '@material-ui/core';
 
 function Example() {
     return (
@@ -7,9 +8,10 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">SPA Setting</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">Complete!</div>
+                        <Button color='primary' variant='contained'>Test Button</Button>
                     </div>
                 </div>
             </div>
@@ -19,6 +21,6 @@ function Example() {
 
 export default Example;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById('app')) {
+    ReactDOM.render(<Example />, document.getElementById('app'));
 }
