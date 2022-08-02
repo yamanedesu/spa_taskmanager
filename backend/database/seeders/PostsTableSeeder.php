a28a458cdda2aee1eb6e721c60be3b79a94c84ac
yamanedesu <test@test.com>
2022-08-02 18:47:02 +0900

    【追加】migrationファイルとseederファイルを追加

---
+++ backend/database/seeders/PostsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('posts')->insert([
+            [
+                'name' => '山内',
+                'content' => 'reactトレーニング',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'SALTO',
+                'content' => '出社',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'テスト',
+                'content' => 'データ投入',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]
+        ]);
+    }
+}
